24f7aed1e889ca579807c61cdeb35ded39461e6d
Manish Chaudhary <cmanish049@gmail.com>
2023-06-05 22:04:33 +0545

    read catalog json

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,33 @@
 // import material dart
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:three/models/catalog.dart';
 import 'package:three/widgets/drawer.dart';
 import 'package:three/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
-  
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
   final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
 
-  HomePage({super.key});
-
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    final productsData = decodedData["products"];
+    print(productsData);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
